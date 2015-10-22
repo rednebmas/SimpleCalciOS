31c5043660ccaf368378079859a4a38ca6d85be6
Sam Bender <rednebmas@gmail.com>
2015-10-22 12:15:48 -0700

    Equal width and heights for buttons

--- SimpleCalciOS/ViewController.swift
+++ SimpleCalciOS/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        self.view.backgroundColor = UIColor.darkGrayColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
 
 }
 
